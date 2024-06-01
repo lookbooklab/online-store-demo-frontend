a7630e5588d4b46cc47e71c3e24839c27546bf8e
Lookbook Lab <team@lookbooklab>
2024-05-31 21:43:02 -0700

    Updated next.config image domains

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["res.cloudinary.com", "localhost"],
+    domains: ["res.cloudinary.com", "localhost", "https://online-store-demo-frontend.vercel.app"],
   },
 };
 
