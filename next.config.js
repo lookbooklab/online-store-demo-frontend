bea911e1ca88058e5eb4dca214b3e50d8750687d
Lookbook Lab <team@lookbooklab>
2024-05-31 21:54:17 -0700

    Experimenting with next.config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["res.cloudinary.com", "localhost", "online-store-demo-frontend.vercel.app"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'online-store-demo-frontend.vercel.app',
+        port: '',
+      },
+    ],
   },
 };
 
