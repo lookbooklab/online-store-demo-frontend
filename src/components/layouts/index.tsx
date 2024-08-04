087dfabab3b0611588252da385ac93c60496d0d6
Lookbook Lab <team@lookbooklab>
2024-08-04 08:56:52 +0800

    Removed Theme Provider for now

--- src/components/layouts/index.tsx
+++ src/components/layouts/index.tsx
@@ -1,15 +1,12 @@
 import Footer from "./footer";
 import Header from "./header";
-import { ThemeProvider } from "@/providers/theme-provider";
 
 export default function LayoutMain({ children }: { children: JSX.Element }) {
   return (
     <>
-      <ThemeProvider>
-        <Header></Header>
-        {children}
-        <Footer></Footer>
-      </ThemeProvider>
+      <Header></Header>
+      {children}
+      <Footer></Footer>
     </>
   );
 }
