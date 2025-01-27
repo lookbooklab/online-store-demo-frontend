0af430ebbe2a5994cffc13f173e203eee7fa9c28
Lawrence Moore <thelawrencemoore@gmail.com>
2025-01-27 10:18:33 -0800

    Put favorites back into the header

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -208,6 +208,24 @@ export default function Header() {
                 </Button>
               </div>
             )}
+            <div className={"absolute right-0 top-1 flex"}>
+              <div className="flex items-center">
+                <div className="cursor-pointer mr-4">
+                  <Wishlist
+                    trigger={
+                      <button className="relative hover:bg-primary hover:text-secondary aspect-square p-1">
+                        <HeartIcon className={"w-6 pt-1 lg:w-8"} />
+                        {wishlistItem.length > 0 && (
+                          <div className="absolute -right-0.5 -top-0.5 text-xs bg-black h-5 w-5 flex items-center justify-center rounded-full text-white">
+                            {wishlistItem.length}
+                          </div>
+                        )}
+                      </button>
+                    }
+                  ></Wishlist>
+                </div>
+              </div>
+            </div>
           </div>
           <NavigationMenu className="hidden md:block max-w-none">
             {menuItems && (
